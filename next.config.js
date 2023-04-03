2fc8f754667329cf6f9e0d3986710fc79c3592d5
MAC <mac@standoski.local>
2023-04-03 22:23:12 +0100

    adsense

--- next.config.js
+++ next.config.js
@@ -17,5 +17,31 @@ module.exports = withPWA({
   typescript: {
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          }
+        ],
+      },
+    ]
+  }
 })
 
